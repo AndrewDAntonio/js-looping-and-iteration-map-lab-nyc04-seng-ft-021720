b27b954eeb38948230b129d5378cab076aa67d64
AndrewDAntonio <github email address>
2020-03-30 11:42:44 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file.
+function lowerCaseDrivers(arr) {
+    return arr.map(function (ele) {return ele.toLowerCase();
+    });
+}
+
+function nameToAttributes(arr) {
+    return arr.map(function (ele) {
+        const eleFirstName = ele.split(" ")[0];
+        const eleLastName = ele.split(" ")[1];
+        return {firstName : eleFirstName, lastName : eleLastName}
+    });
+}
+
+function attributesToPhrase(arr) {
+    return arr.map(function (ele) {
+        return `${ele.name} is from ${ele.hometown}`
+    });
+}
